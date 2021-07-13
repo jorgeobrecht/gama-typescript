0d904e87e7ddb447d1cf75163ff28b94e9d9b94b
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-13 14:54:03 -0300

    Instalando e Usando o TypeScript

---
+++ primeiro-exemplo.js
@@ -0,0 +1,7 @@
+"use strict";
+function somar(n1, n2) {
+    return n1 + n2;
+}
+var numero1 = 10;
+var numero2 = 5;
+console.log(somar(numero1, numero2));
---
+++ primeiro-exemplo.ts
@@ -0,0 +1,8 @@
+function somar(n1:number,n2:number){
+    return n1+n2
+}
+
+const numero1 = 10;
+const numero2 = 5;
+
+console.log(somar(numero1,numero2))
