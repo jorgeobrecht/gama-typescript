f53e8abcf3f8a8299505d1943fd9f797c2f1550b
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-13 16:39:40 -0300

    Classes

---
+++ classes.ts
@@ -0,0 +1,16 @@
+class Data{
+    /* public dia:number;
+    mes:number;
+    ano:number; */
+
+    constructor(public dia:number,public mes:number,public ano:number = 1970){
+        /* this.dia=dia;
+        this.mes=mes;
+        this.ano=ano; */
+    }
+}
+
+const data=new Data(1,1,2020);
+console.log(data.dia);
+
+const data2=new Data(1,1);
