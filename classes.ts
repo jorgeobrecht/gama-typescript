c027699b7ea42c8466e0b541c9b089a69066e928
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-13 17:03:30 -0300

    Utilizando Modificadores de Acesso

--- classes.ts
+++ classes.ts
@@ -14,3 +14,33 @@ const data=new Data(1,1,2020);
 console.log(data.dia);
 
 const data2=new Data(1,1);
+
+
+class Carro{
+    private velocidadeAtual: number=0;
+
+    constructor(
+        public marca: string,
+        public modelo:string,
+        private velocidadeMax:number=220
+
+    ){}
+
+    private alterarVelocidade(delta:number){
+        if(this.velocidadeAtual+delta<this.velocidadeMax && (this.velocidadeAtual+delta>=0))
+        this.velocidadeAtual+=delta;
+    }
+
+    acelerar(){
+        this.alterarVelocidade(5);
+        }
+    
+
+    frear(){
+        this.alterarVelocidade(-5);
+    }
+        
+}
+
+const carro=new Carro('Fiat','Uno',250);
+carro.acelerar();
